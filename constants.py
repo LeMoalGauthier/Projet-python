c61806fc22b2fb3cce6845a98832c9545765332d
LeMoalGauthier <gauthier.le-moal@isen-ouest.yncrea.fr>
2024-01-26 14:39:47 +0100

    Création du fichier contants.py

---
+++ constants.py
@@ -0,0 +1,12 @@
+WHITE = (255, 255, 255)
+RED = (255, 0, 0)
+GREEN = (0, 255, 0)
+BLUE = (0, 0, 255)
+BLACK = (0, 0, 0)
+
+# Définir les dimensions de la fenêtre
+SCREEN_WIDTH = 1200
+SCREEN_HEIGHT = 700
+
+# Couleur background
+BACKGROUND_COLOR = (0,0,0)
