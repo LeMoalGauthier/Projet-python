81dd113fc31a9f71fd13dabbc3eb4b7a7b35ee35
LeMoalGauthier <gauthier.le-moal@isen-ouest.yncrea.fr>
2024-02-02 14:37:42 +0100

    Maj 02/02/24

--- entity.py
+++ entity.py
@@ -45,4 +45,50 @@ class Entity(object):
         return False
 
     def reverseDirection(self):
-        self.direction =
+        self.direction *= -1
+        temp = self.node
+        self.node = self.target
+        self.target = temp
+
+    def oppositeDirection(self, direction):
+        if direction is not STOP:
+            if direction == self.direction * -1:
+                return True
+            return False
+
+    def setSpeed(self, speed):
+        self.speed = speed * TILEWIDTH /16
+
+    def update(self, dt):
+        self.position += self.directions[self.direction]*self.speed*dt
+
+        if self.overshotTarget():
+            self.node = self.target
+            directions = self.validDirections()
+            direction = self.randomDirection(directions)
+            if not self.disablePortal:
+                if self.node.neighbors[PORTAL] is not None:
+                    self.node = self.node.neighbors[PORTAL]
+                self.target = self.getNewTarget(direction)
+                if self.target is not self.node:
+                    self.direction = direction
+                else:
+                    self.target = self.getNewTarget(direction)
+                self.setPosition()
+    def validDirections(self):
+        directions = []
+        for key in [UP, DOWN, LEFT, RIGHT]:
+            if self.validDirection(key):
+                if key != self.direction * -1:
+                    directions.append(key)
+        if len(directions) == 0:
+            directions.append(self.direction * -1)
+        return directions
+
+    def randomDirection(self, directions):
+        return directions[randint(0, len(directions) - 1)]
+    
+    def render(self, screen):
+        if self.visible:
+            p = self.position.asInt()
+            pygame.draw.circle(screen, self.color, p, self.radius)
--- run.py
+++ run.py
@@ -1,13 +1,13 @@
 import pygame
 from pygame.locals import *
 from constants import *
-from nodes import NodeGroup
 from pacman import Pacman
+from nodes import NodeGroup
+from ghosts import GhostGroup
 
 
 class GameController(object):
     def __init__(self):
-        self.nodes = None
         pygame.init()
         self.screen = pygame.display.set_mode(SCREENSIZE, 0, 32)
         self.background = None
@@ -21,22 +21,31 @@ class GameController(object):
         self.setBackground()
         self.nodes = NodeGroup("carte2.txt")
         self.pacman = Pacman(self.nodes.getStartTempNode())
-
+        self.ghosts = GhostGroup(self.nodes.getStartTempNode(), self.pacman)
+        self.ghosts.setSpawnNode(self.nodes.getNodeFromTiles(2+11.5, 3+14))
     def update(self):
         dt = self.clock.tick(30) / 1000.0
         self.pacman.update(dt)
+        self.ghosts.update(dt)
         self.checkEvents()
         self.render()
+
+    def checkPelletEvents(self):
+        pellet = self.pacman.eatPellets(self.pellets.pelletList)
+        if pellet:
+            self.pellets.pelletList.remove(pellet)
+            if pellet.name is POWERPELLET:
+                self.ghosts.startFreight()
 
     def checkEvents(self):
         for event in pygame.event.get():
             if event.type == QUIT:
                 exit()
-
     def render(self):
         self.screen.blit(self.background, (0, 0))
         self.nodes.render(self.screen)
         self.pacman.render(self.screen)
+        self.ghosts.render(self.screen)
         pygame.display.update()
 
 
